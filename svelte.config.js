6d09e0598edefbe8ff2cdcd38df7aea4bee52c5d
Kylie Koopman <kylie.koopman@fullscript.com>
2022-08-11 18:02:17 -0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 const dev = process.env.NODE_ENV === 'development';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: preprocess({
-        postcss: {
-            plugins: [tailwind, autoprefixer]
-        }
-    }),
+    preprocess: preprocess(),
 
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
